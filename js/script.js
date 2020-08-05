a62dd00e2de0b28e344d4f34b3f850efcbd04421
danclp <danc@buncreative.com>
2020-08-06 00:25:06 +0800

    First commit of source files

---
+++ js/script.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function($) {
+
+	$('.category').hover(function() {
+		$(this).children('.category_cover').css('z-index','5');
+	}, function() {
+		$(this).children('.category_cover').css('z-index','1');
+	});
+
+	// Popup
+	$(".openfeed").magnificPopup({
+		type: "inline",
+		midClick: true,
+		closeBtnInside: true,
+		alignTop: false
+	});
+
+	// Validation
+	$.validator.addMethod("emailfull", function(value, element) {
+		return this.optional(element) || /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test(value);
+	}, "Please enter valid email address!");
+
+	$.validator.addMethod('nofreeemail', function (value) {
+		return /^([\w-.]+@(?!gmail\.com)(?!yahoo\.com)(?!hotmail\.com)(?!outlook\.com)([\w-]+.)+[\w-]{2,4})?$/.test(value); 
+	}, 'Please key in company email address only');
+
+	$('#eForm').validate({
+		rules: {
+			email: {
+				required: true,
+				email: true,
+				emailfull: true,
+				nofreeemail: true
+			},
+			contact: {
+				required: true,
+				digits: true,
+				minlength: 8
+			}
+		}
+	});
+
+	$('#eForm').submit(function() {
+		if($('#checkbox input:checked').length < 1){
+			$('#checkboxalert').show()
+			$('#checkboxalert').html("Please select at least 1 answer");
+			
+			return false;
+		}else{
+			$('#checkboxalert').hide();
+		}
+	});
+});
